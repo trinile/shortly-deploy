bcd44a739a3dbc16e36405606b6b2abedbf90579
Trini Le <trinile@gmail.com>
2016-03-31 11:12:44 -0700

    adjust of client concat grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,9 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['public/lib/jquery.js', 
-        'public/lib/underscore.js', 'public/lib/backbone.js', 'public/lib/handlebars.js', 'public/client/**/*.js'],
+        src: ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/backbone.js', 'public/lib/handlebars.js', 
+        'public/client/app.js', 'public/client/link.js', 'public/client/links.js', 'public/client/linkView.js', 
+        'public/client/linksView.js', 'public/client/createLinkView.js', 'public/client/router.js'],
         dest: 'public/dist/<%= pkg.name %>.js'
       }
     },
